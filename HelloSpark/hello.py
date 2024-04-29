05a604c840e81ab40becfba83b5175d4989f335b
Sathvik <sathvikng56@gmail.com>
2024-04-29 12:52:16 +0530

    First spark app

---
+++ HelloSpark/hello.py
@@ -0,0 +1,13 @@
+from pyspark.sql import *
+from lib.logger import Log4j
+
+if __name__ == "__main__":
+    spark = SparkSession.builder \
+                .appName("HelloSpark") \
+                .master("local[3]") \
+                .getOrCreate()
+
+    logger = Log4j(spark)
+
+    logger.info("Starting HelloSpark")
+    logger.info("Finished HelloSpark")
